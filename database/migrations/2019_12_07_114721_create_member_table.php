cd657b0495852ace85f3be212ce57070ea6e16f2
OFTheSun <joezaza999@hotmail.com>
2019-12-07 18:53:36 +0700

    สร้างตาราง member และ position

---
+++ database/migrations/2019_12_07_114721_create_member_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemberTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('position_id')->unsigned();
+            $table->foreign('position_id')->references('id')->on('position');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member');
+    }
+}
